2ce65571e2c1bb7f5fdd07b98fad54638703a78e
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-07-14 17:23:46 -0400

    7/14/2024

--- src/main/java/Advanced_OOP/Main.java
+++ src/main/java/Advanced_OOP/Main.java
@@ -3,14 +3,19 @@ package Advanced_OOP;
 public class Main {
     public static void main(String[] args) {
 
-        BankAccount bankAccount = new SavingsAccount("Savings account", "John Brown", 100000, 4.2);
-        System.out.println("Initial savings account details are: \n" + bankAccount.getCustomer_name() + "\n" + bankAccount.getAccount_type() + "\n" +
-                bankAccount.balance);
+        BankAccount savingsAccount = new SavingsAccount("Savings account", "John Brown", 100000, 4.2);
+        System.out.println("Initial savings account details are: \n" + savingsAccount.getCustomer_name() + "\n" + savingsAccount.getAccount_type() + "\n" +
+                savingsAccount.getBalance());
 
-        BankAccount bankAccount2 = new CheckingAccount("Checking account", "Olive Johns", 100050, 24);
+        BankAccount checkingAccount = new CheckingAccount("Checking account", "Olive Johns", 100050, 24);
 
-        bankAccount2.withdrawal(500);
-        System.out.println("Current balance after withdrawal is: "+ bankAccount2.getBalance());
+        checkingAccount.withdrawal(500);
+        System.out.println("Current balance after withdrawal is: "+ checkingAccount.getBalance());
 
+        savingsAccount.deposit(1000);
+        System.out.println("Balance after deposit: "+ savingsAccount.getBalance());
+
+        SavingsAccount savings = (SavingsAccount) savingsAccount;
+        savings.calculate_interest();
     }
 }
