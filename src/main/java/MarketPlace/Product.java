f4ba73d7a770066efbad78c4cb8b66b27cf86b05
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-07-14 18:23:48 -0400

    7/14/2024

--- src/main/java/MarketPlace/Product.java
+++ src/main/java/MarketPlace/Product.java
@@ -1,4 +1,36 @@
 package MarketPlace;
 
 public class Product {
+
+    private String typeOfProduct;
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(String typeOfProduct, double price) {
+        this.typeOfProduct = typeOfProduct;
+        this.price = price;
+    }
+
+    public String getTypeOfProduct() {
+        return typeOfProduct;
+    }
+
+    public void setTypeOfProduct(String typeOfProduct) {
+        this.typeOfProduct = typeOfProduct;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Type of product is: " + typeOfProduct + "\nPrice of product is: " + price;
+    }
 }
