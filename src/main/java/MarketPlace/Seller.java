145ae5415c63b7765d96ab8ca15e68de54a9380e
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-07-14 18:11:59 -0400

    7/14/2024

--- src/main/java/MarketPlace/Seller.java
+++ src/main/java/MarketPlace/Seller.java
@@ -1,4 +1,35 @@
 package MarketPlace;
 
-public class Seller {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Seller extends Person {
+    private List<Product> products;
+
+    public Seller() {
+    }
+
+    public Seller(String name, String email) {
+        super(name, email);
+        this.products = new ArrayList<>();
+    }
+
+    //add product
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    //display products
+    public void displayProducts() {
+        System.out.println("These are products of seller " + getName() + ":");
+        for (Product product : products) {
+            System.out.println("This is product: " + product);
+        }
+    }
+
+    @Override
+    public void displayRole() {
+        System.out.println("Hi, I am a seller");
+    }
 }
