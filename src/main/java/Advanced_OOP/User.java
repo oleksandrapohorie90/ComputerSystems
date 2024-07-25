d850cea753dc65c4c8c44f2d878fecb500dc4b0e
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-07-24 21:45:30 -0400

    7/24/2024

---
+++ src/main/java/Advanced_OOP/User.java
@@ -0,0 +1,28 @@
+package Advanced_OOP;
+
+public class User {
+
+    public void openAccount(BankAccount account){
+
+    }
+
+    public void seeAccount(){
+
+    }
+
+    public void deposit(){
+
+    }
+
+    public void withdraw(){
+
+    }
+
+    public void changePinOnAccount(BankAccount account){
+
+    }
+
+
+
+
+}
